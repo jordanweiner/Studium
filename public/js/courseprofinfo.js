2af6b32b60199a5e51ced4b935036ac646edcd7e
jordanweiner <jweiner@ucsd.edu>
2019-02-12 19:28:13 -0800

    can now read JSON file

--- public/js/courseprofinfo.js
+++ public/js/courseprofinfo.js
@@ -1,3 +1,7 @@
+$.getJSON( "data.json", function( json ) {
+    console.log( "JSON Data received, name is " + json.name);
+});
+
 function getUserSearchInput() {
 	var x = document.getElementById("search-input").value;
 	console.log("search-input is: " + x);
