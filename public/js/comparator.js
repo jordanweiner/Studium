7c0a979d1898c221dbe4305c48a83de806642f04
Karl Chen <klc026@ucsd.edu>
2019-02-26 18:42:18 -0800

    comparatorprogress

--- public/js/comparator.js
+++ public/js/comparator.js
@@ -1,3 +1,13 @@
 function goBack() {
 	window.history.back();
 }
+
+function lnameLetterButtonPressed() {
+	var chosenletter = document.getElementById('lnameletter');
+	var value = chosenletter[chosenletter.selectedIndex].value;
+	$(".container comparator-page").hide();
+}
+
+$(".but").click(lnameLetterButtonPressed);
+
+document.getElementById('lnameprof').addEventListener('click',lnameLetterButtonPressed);
--- routes/comparator.js
+++ routes/comparator.js
@@ -1,5 +1,9 @@
 exports.viewProject = function(req, res) { 
 	
 	var cleandata = require('../data/cleandata');
+
 	res.render("comparator", cleandata);
 };
+
+
+
