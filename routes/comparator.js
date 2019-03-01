1ffc62fd08b938bf18167156b0d95dddb34bd741
jordanweiner <jweiner@ucsd.edu>
2019-02-28 19:51:17 -0800

    jordan's google analytics

--- routes/comparator.js
+++ routes/comparator.js
@@ -1,9 +1,11 @@
-exports.viewProject = function(req, res) { 
-	
-	var cleandata = require('../data/cleandata');
+var cleandata = require('../data/cleandata.json');
 
+exports.viewProject = function(req, res) { 
+	cleandata["viewAlt"] = false;
 	res.render("comparator", cleandata);
 };
 
-
-
+exports.viewAlt = function(request, response){
+	cleandata["viewAlt"] = true;
+	res.render("comparator", cleandata);
+};
