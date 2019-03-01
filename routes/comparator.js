a6111edece215179cb9c25919015298bf4d49b81
jordanweiner <jweiner@ucsd.edu>
2019-02-28 20:20:18 -0800

    working

--- routes/comparator.js
+++ routes/comparator.js
@@ -1,11 +1,11 @@
 var cleandata = require('../data/cleandata.json');
 
-exports.viewProject = function(req, res) { 
+exports.view = function(req, res) { 
 	cleandata["viewAlt"] = false;
 	res.render("comparator", cleandata);
 };
 
-exports.viewAlt = function(request, response){
+exports.viewAlt = function(req, res){
 	cleandata["viewAlt"] = true;
 	res.render("comparator", cleandata);
 };
